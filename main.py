a44dcfd63a065b073ba183e96689de7a527158cf
DCCCGAMELAB <incorporatedaniyal@yahoo.com>
2023-03-12 21:41:46 +0400

    Added startup sequence to main file

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+# author : daniyal karimi 
+import time
 import datetime
 
-print(datetime.datetime.now())
-print("Hello World")
-print("Daniyal has a small as dick")
+def startupgraphic():
+    print("Hello world")
+    print("\n" * 40)
+    time.sleep(2)
+    print("Designed for ease of data analysis and support to help teachers")
+    print("A program that incorporates all the wants of a teacher when it comes to stastiscal analysis of student grades")
+    print("Dedicated to Mr Rawat")
+    print("\n" * 40)
+    time.sleep(7)
+    print("Initial idea was discovered 22/05/2021")
+    print("Development began on 07/06/2023")
+    time.sleep(4)
+    print("An Abdollah Aali Hosseini and Daniyal Karimi production")
+    time.sleep(3)
+    print("\n" * 40)
+    print("Data")
+    time.sleep(1)
+    print("analysis")
+    time.sleep(0.35)
+    print("and")
+    time.sleep(0.13)
+    print("storage")
+    time.sleep(2)
+    print("application")
+    time.sleep(5)
+    print("\n" * 41)
+    print("The D.A.S.A")
+    
+startupgraphic()
